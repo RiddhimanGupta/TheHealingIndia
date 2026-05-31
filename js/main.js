9dc287a6add1813a9e12345dbb07bf9db19144f9
Riddhiman Gupta <riddhimangupta@gmail.com>
2026-05-31 16:10:20 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // js/main.js
 import { initUI, go, toast } from './ui.js';
 import { initSensors, triggerSOS, cancelSOS } from './sensors.js';
+import { lookupChallan, quickLook, openPay, closePay, selPM, confirmPay, disputeIt } from './challan.js';
 // import { initMap, centerOnUser } from './map.js';
 // import { lookupChallan, quickLook } from './challan.js';
 
@@ -19,6 +20,13 @@ window.go = (tab) => {
 window.toast = toast;
 window.triggerSOS = triggerSOS;
 window.cancelSOS = cancelSOS;
+window.lookupChallan = lookupChallan;
+window.quickLook = quickLook;
+window.openPay = openPay;
+window.closePay = closePay;
+window.selPM = selPM;
+window.confirmPay = confirmPay;
+window.disputeIt = disputeIt;
 // window.lookupChallan = lookupChallan;
 // window.centerOnUser = centerOnUser;
 // ... map the rest of your button actions here ...
